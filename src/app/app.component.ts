620e3258ccf71ee0dcef3e8c6faaeffe3a2198e2
Marcin Wronecki <marcin.wronecki@gmail.com>
2021-09-20 06:25:21 +0200

    daily

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  entireVerses = 'Fragmnet: Mat 8';
+
+  dayOfWeek: string = "";
+
+  ngOnInit() {
+    const today = new Date();
+    this.dayOfWeek = today.toLocaleDateString(`pl-PL`,{weekday: 'long'});
+  }
 }
